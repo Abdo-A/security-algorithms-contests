477e53516f47d089c1a45fee6c7bf1c80d0683c9
Abdo <abdopoetry@gmail.com>
2018-12-23 20:07:42 +0200

    Add reverse-permutation-box algorithm

---
+++ des-algorithms/reverse-permutation-box.py
@@ -0,0 +1,35 @@
+inputSize = int(input())
+
+permutationBox = input()
+permutationBox = permutationBox.split()
+
+
+# converting items in the table to be numbers
+for i, item in enumerate(permutationBox):
+    permutationBox[i] = int(permutationBox[i])
+
+impossible = False
+
+# checking for error
+passedNumbersArrayCheck = []
+
+for i, item in enumerate(permutationBox):
+    for item in passedNumbersArrayCheck:
+        if item == permutationBox[i]:
+            impossible = True
+
+    passedNumbersArrayCheck.append(permutationBox[i])
+
+# getting reversePermutationBox
+reversePermutationBox = [None] * inputSize
+if(not impossible):
+
+    for i, item in enumerate(permutationBox):
+        reversePermutationBox[i] = permutationBox.index(i+1)+1
+
+    reversePermutationBoxString = ' '.join(
+        str(n) for n in reversePermutationBox)
+
+    print(reversePermutationBoxString)
+else:
+    print("IMPOSSIBLE")
